a5e513df377bcda6581b9e800da8df10a0753d10
Pandit upase <panditupase1@gmail.com>
2026-05-07 12:00:34 +0530

    Objects Exposure

---
+++ Objects2.js
@@ -0,0 +1,25 @@
+// Create object
+const user = {
+
+    name: "Pandit",
+    age: 20
+
+};
+
+
+// Create function
+// userObj is parameter
+function printUser(userObj){
+
+    // Print object name
+    console.log(userObj.name);
+
+    // Print object age
+    console.log(userObj.age);
+
+}
+
+
+// Pass user object into function
+// user is argument
+printUser(user);
