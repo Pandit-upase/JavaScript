3bbcda4212363ebe2e4ce1c18afc38d61ca622fd
Pandit upase <panditupase1@gmail.com>
2026-05-11 16:08:07 +0530

    Break&continue

---
+++ breakcontinue.js
@@ -0,0 +1,21 @@
+// Loop from 1 to 10
+for(let i = 1; i <= 10; i++){
+
+    // Skip 3
+    if(i === 3){
+
+        continue;
+
+    }
+
+    // Stop loop at 7
+    if(i === 7){
+
+        break;
+
+    }
+
+    // Print value
+    console.log(i);
+
+}
