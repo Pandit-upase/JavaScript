90c680fe4c192959a00b614de0096ee478d4e413
Pandit upase <panditupase1@gmail.com>
2026-05-11 16:08:44 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,36 @@
+/*//  For Loop from 1 to 5
+for(let i = 1; i <= 5; i++){
+
+    // Print current value
+    console.log(i);
+}
+
+//While loop
+
+// Starting value
+let i = 1;
+// Loop while true
+while(i <= 3){
+
+    // Print value
+    console.log(i);
+
+    // Increase value
+    i++;
+}*/
+
+
+//do-while
+
+// Starting value
+let j= 10;
+
+// Run once first
+do{
+     
+    console.log(j);
+
+    j++;
+
+}
+while(j <= 3);
