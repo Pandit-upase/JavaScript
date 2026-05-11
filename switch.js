9161947d9e0d5db662b1c5b85191d4a5777eb256
Pandit upase <panditupase1@gmail.com>
2026-05-11 16:08:55 +0530

    switch case

---
+++ switch.js
@@ -0,0 +1,22 @@
+// Day number
+let day = 2;
+
+// Switch checks value
+switch(day){
+
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    case 3:
+        console.log("Wednesday");
+        break;
+
+    default:
+        console.log("Invalid Day");
+
+}
